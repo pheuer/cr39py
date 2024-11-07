4d9a598c3a044dd6b85fd81ed714d6174da1ea24
Peter Heuer <pvheuer@gmail.com>
2024-11-07 14:42:44 -0500

    Docstrings

--- src/cr39py/subset.py
+++ src/cr39py/subset.py
@@ -4,33 +4,33 @@ from cr39py.cut import Cut
 
 class Subset(ExportableClassMixin):
     """
-    A subset of the track data. The subset is defined by a domain, a list
+    A subset of the track data.
+
+    The subset is defined by a domain, a list
     of cuts, and a number of diameter slices (Dslices).
 
-    Paramters
-    ---------
+    Parameters
+    ----------
 
-    grp : h5py.Group or string file path
-        An h5py Group or file path to an h5py file from which to
-        load the subset
+    domain : `~cr39py.cut.Cut`
+        A cut that defines the domain of the subset. The domain is the area in parameter
+        space the subset encompasses. This could limit the subset to a region in space
+        (e.g. x=[-5, 0]) or another parameter (e.g. D=[0,20]). The domain is represented
+        by a Cut object, but it is inclusive rather than exclusive.
+
+    ndslices : int
+        Number of bins in the diameter axis to slice the data into.
+        The default is 1.
+
 
     Notes
     -----
 
-    Domain
-    ------
-    The domain is the area in parameter space the subset encompasses. This
-    could limit the subset to a region in space (e.g. x=[-5, 0]) or another
-    parameter (e.g. D=[0,20]). The domain is represented by a cut, but it is
-    an inclusive rather than an exclusive cut.
 
-    List of Cuts
-    ------------
+
     The subset includes a list of cuts that are used to exclude tracks that
     would otherwise be included in the domain.
 
-    DSlices
-    ------
     Track diameter is proportional to particle energy, so slicing the subset
     into bins sorted by diameter sorts the tracks by diameter. Slices are
     created by equally partitioining the tracks in the subset into some
@@ -39,7 +39,7 @@ class Subset(ExportableClassMixin):
 
     _exportable_attributes = ["cuts", "domain", "ndslices", "current_dslice_index"]
 
-    def __init__(self, *args, domain=None, ndslices=None):
+    def __init__(self, domain=None, ndslices=None):
 
         self.cuts = []
         self.ndslices = ndslices
@@ -59,6 +59,12 @@ class Subset(ExportableClassMixin):
         self.current_dslice_index = 0
 
     def __eq__(self, other):
+        """
+        Determines whether two subsets are equal.
+
+        Two subsets are defined to be equal if they have equal
+        domains, cuts, and numbers of dslices.
+        """
 
         if not isinstance(other, Subset):
             return False
@@ -74,7 +80,8 @@ class Subset(ExportableClassMixin):
 
         return True
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """String representation of the Subset"""
         s = ""
         s += "Domain:" + str(self.domain) + "\n"
         s += "Current cuts:\n"
@@ -89,6 +96,7 @@ class Subset(ExportableClassMixin):
         return s
 
     def __hash__(self):
+        """Hash of the Subset"""
         s = "domain:" + str(hash(self.domain))
         s += "ndslices:" + str(self.ndslices)
         s += "current_dslice_index" + str(self.current_dslice_index)
@@ -97,26 +105,54 @@ class Subset(ExportableClassMixin):
 
         return hash(s)
 
-    def set_domain(self, cut):
+    def set_domain(self, domain: Cut | None) -> None:
         """
-        Sets the domain cut: an inclusive cut that will not be inverted
-        """
-        self.domain = cut
+        Sets the domain cut
 
-    def select_dslice(self, i):
-        if i is None:
+        Parameters
+        ----------
+        domain : `~cr39py.cut.Cut` | None
+            The domain, represented as a Cut object.
+            If None, the domain is set to an empty
+            domain.
+
+        """
+        if domain is None:
+            self.domain = Cut()
+
+        self.domain = domain
+
+    def select_dslice(self, dslice: int | None) -> None:
+        """Set the currently selected dslice.
+
+        Parameters
+        ----------
+
+        dslice: int|None
+            The dslice to select. If None, then all dslices
+            will be selected.
+
+        """
+        if dslice is None:
             self.current_dslice_index = None
-        elif i > self.ndslices - 1:
+        elif dslice > self.ndslices - 1:
             raise ValueError(
-                f"Cannot select the {i} dslice, there are only "
+                f"Cannot select the {dslice} dslice, there are only "
                 f"{self.ndslices} dslices."
             )
         else:
-            self.current_dslice_index = i
+            self.current_dslice_index = dslice
 
     def set_ndslices(self, ndslices):
         """
         Sets the number of ndslices
+
+        Parameters
+        ----------
+
+        ndslices : int
+            Number of dslices
+
         """
         if not isinstance(ndslices, int) or ndslices < 0:
             raise ValueError(
@@ -124,17 +160,52 @@ class Subset(ExportableClassMixin):
                 f"was {ndslices}"
             )
 
-        else:
-            self.ndslices = int(ndslices)
+        self.ndslices = int(ndslices)
+
+        # Ensure that, when changing the number of dslices,
+        # you don't end up with a current_dslice_index that exceeds
+        # the new number of dslices
+        if self.current_dslice_index > self.ndslices - 1:
+            self.current_dslice_index = self.ndslices - 1
 
     # ************************************************************************
     # Methods for managing cut list
     # ************************************************************************
     @property
     def ncuts(self):
+        """Number of current cuts on this subset"""
         return len(self.cuts)
 
-    def add_cut(self, *args, **kwargs):
+    def add_cut(self, *args, **kwargs) -> None:
+        """
+        Adds a new Cut to the Subset.
+
+        Either provide a single Cut (as an argument), or any combination of the
+        cut keywords to create a new cut.
+
+        Parameters
+        ----------
+
+        cut : `~cr39py.cut.Cut`
+            A single cut to be added, as an argument.
+
+
+        xmin, xmax, ymin, ymax, cmin, cmax, dmin, dmax, emin, emax : float
+            Keywords defining a new cut. Default for all keywords is None.
+
+        Examples
+        --------
+
+        Create a cut, then add it to the subset
+        .. code-block:: python
+            cut = Cut(cmin=30)
+            subset.add_cut(cut)
+
+        Or create a new cut on the subset automatically.
+        .. code-block:: python
+            subset.add_cut(cmin=30)
+
+        """
 
         if len(args) == 1:
             c = args[0]
@@ -143,7 +214,20 @@ class Subset(ExportableClassMixin):
 
         self.cuts.append(c)
 
-    def remove_cut(self, i):
+    def remove_cut(self, i: int) -> None:
+        """Remove an existing cut from the subset.
+
+        The cut will be removed from the list, so the index of latter
+        cuts in the list will be decremented.
+
+        Parameters
+        ----------
+
+        i : int
+            Index of the cut to remove
+
+        """
+
         if i > len(self.cuts) - 1:
             print(
                 f"Cannot remove the {i} cut, there are only " f"{len(self.cuts)} cuts."
@@ -151,10 +235,19 @@ class Subset(ExportableClassMixin):
         else:
             self.cuts.pop(i)
 
-    def replace_cut(self, i, c):
+    def replace_cut(self, i: int, cut: Cut):
+        """Replace the ith cut in the Subset list with a new cut
+
+        Parameters
+        ----------
+        i : int
+            Index of the Cut to replace.
+        cut : `cr39py.cut.Cut`
+            New cut to insert.
+        """
         if i > len(self.cuts) - 1:
             print(
                 f"Cannot replace the {i} cut, there are only " f"{len(self.cuts)} cuts."
             )
         else:
-            self.cuts[i] = c
+            self.cuts[i] = cut
