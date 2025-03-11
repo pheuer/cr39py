423ad6fb5de5660b66551c68a55f532c3e299e51
Peter Heuer <pvheuer@gmail.com>
2025-03-11 09:50:38 -0400

    Add an explicit test on read_cpsa

--- tests/scan/test_cpsa.py
+++ tests/scan/test_cpsa.py
@@ -4,9 +4,11 @@ Tests for cpsa.py
 
 from pathlib import Path
 
+import numpy as np
 import pytest
 
-from cr39py.scan.cpsa import extract_etch_time
+from cr39py.core.data import data_dir
+from cr39py.scan.cpsa import extract_etch_time, read_cpsa
 
 
 @pytest.mark.parametrize(
@@ -16,3 +18,14 @@ from cr39py.scan.cpsa import extract_etch_time
 def test_extract_etch_time(etch_time_str, time):
     path = Path(f"{etch_time_str}.cpsa")
     assert extract_etch_time(path) == time
+
+
+def test_read_cpsa():
+    path = data_dir / Path("test/test_alphas.cpsa")
+    tracks, metadata = read_cpsa(path)
+
+    assert isinstance(tracks, np.ndarray)
+    assert tracks.shape[-1] == 6
+
+    assert isinstance(metadata, dict)
+    assert "nframes" in metadata
