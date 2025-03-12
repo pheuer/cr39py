b9a4cb465cd0af19b134325e272ab8ff66e51485
Peter Heuer <pvheuer@gmail.com>
2025-03-12 12:48:59 -0400

    Remove print

--- src/cr39py/core/data.py
+++ src/cr39py/core/data.py
@@ -3,4 +3,3 @@ from pathlib import Path
 
 # data_dir = Path(importlib.resources.files("cr39py")).parent.parent / Path("data")
 data_dir = Path(__file__).parent.parent.parent.parent / Path("data")
-print(data_dir)
