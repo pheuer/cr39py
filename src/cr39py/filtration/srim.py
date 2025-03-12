2e29fc1644059a692de31dd4ed4315babeea0996
Peter Heuer <pvheuer@gmail.com>
2025-03-12 13:00:34 -0400

    Clear notebook contents, edit docstring references to data_dir

--- src/cr39py/filtration/srim.py
+++ src/cr39py/filtration/srim.py
@@ -56,7 +56,7 @@ class SRIMData:
         The SRIM file must have stopping power saved in units of keV/um.
 
         SRIM data files must be placed in the directory
-        ``cr39py/data/srim``.
+        ``src/cr39py/core/data/srim``.
 
         The filename must follow the format ``[particle] in [target material].txt``
 
