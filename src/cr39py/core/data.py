54659b2682f7aae3bb95473b3aa9374993e0a652
Peter Heuer <pvheuer@gmail.com>
2025-03-12 12:54:29 -0400

    Relocate data directory within src/cr39py

--- src/cr39py/core/data.py
+++ src/cr39py/core/data.py
@@ -2,4 +2,4 @@ import importlib.resources
 from pathlib import Path
 
 # data_dir = Path(importlib.resources.files("cr39py")).parent.parent / Path("data")
-data_dir = Path(__file__).parent.parent.parent.parent / Path("data")
+data_dir = Path(__file__).parent / Path("data")
