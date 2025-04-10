9825c937de9d058d0a1f62bcb735a862ca593555
pheuer <pvheuer@gmail.com>
2025-04-10 11:42:07 -0400

    Add another optimizer test

--- tests/etch/test_optimize.py
+++ tests/etch/test_optimize.py
@@ -10,14 +10,26 @@ from cr39py.core.ci import SilentPlotting
 from cr39py.etch.optimize import EtchTimeOptimizer
 
 
-def test_run_optimizer():
+@pytest.fixture
+def optimizer():
     mean_energy = 15.2 * u.MeV  # Mean energy in MeV
     std_dev_energy = 0.4 * u.MeV  # Standard deviation in MeV
     filters = Stack.from_string("15 um Ta, 1500 um CR-39, 180 um Al")
 
     opt = EtchTimeOptimizer(mean_energy, std_dev_energy, filters)
+    return opt
 
+
+def test_run_optimizer(optimizer):
     with SilentPlotting():
-        optimum = opt.optimal_etch_time(3e4, plot=True, overlap_max=0.1)
+        optimum = optimizer.optimal_etch_time(3e4, plot=True, overlap_max=0.1)
 
     assert np.isclose(optimum / 60, 6.41, atol=0.25)
+
+
+def test_no_optimal_time_exists(optimizer):
+    """
+    No possible etch time can meet this criteria - so a value error should be raised
+    """
+    with pytest.raises(ValueError):
+        optimum = optimizer.optimal_etch_time(1e6, plot=False, overlap_max=0.005)
